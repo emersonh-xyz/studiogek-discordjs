10137fba696d2a4378c99868908bf854df943edd
Emerson <70560728+emersonh-xyz@users.noreply.github.com>
2023-03-13 22:53:05 -0400

    Update checkVideo.js

--- src/functions/checkVideo.js
+++ src/functions/checkVideo.js
@@ -11,6 +11,8 @@ module.exports = (client) => {
 
 
     client.checkVideo = async () => {
+
+        console.log("📺 | Checking for new videos! ")
         const data = await parser.parseURL(
             "https://youtube.com/feeds/videos.xml?channel_id=UCvdcBgHnd2WgJoP1irXxg1g"
         )
@@ -24,23 +26,37 @@ module.exports = (client) => {
 
         if (jsonData.id !== data.items[0].id) {
 
-            console.log("New video found...")
-
-            fs.writeFileSync(`${__dirname}/../json/video.json`, JSON.stringify({ id: data.items[0].id }));
-
-            const guild = await client.guilds
-                .cache.get(guildId)
-
-            const channel = await guild.channels
-                .fetch("1084327119259193344")
 
 
             const { title, link, id, author } = data.items[0]
 
-            await channel.send(
-                `@here New upload! \n\n**${title}**
+            console.log(`✅ | New video found ${id}`)
+
+            const guild = await client.guilds
+                .cache.get(guildId);
+
+
+            // Fetch the upload channel
+            const uploadChannel = await guild.channels
+                .fetch("1084327119259193344");
+
+
+
+            // Fetch the logs channel
+            const logs = await guild.channels
+                .fetch("1084318629841088522");
+
+            await logs.send(`**Log**: New video found \n**Title:** ${title} \n**Pushing to**: <#1084327119259193344>`)
+
+            fs.writeFileSync(`${__dirname}/../json/video.json`, JSON.stringify({ id: data.items[0].id }));
+
+            await uploadChannel.send(
+                `New upload! \n\n**${title}**
                 \n ${link}`
             )
+
+        } else {
+            console.log("❌ | No new videos were found")
         }
     }
 };

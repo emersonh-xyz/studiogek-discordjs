603d589a4791524b3a8d9b016d8cfdba6b14eb2d
Emerson <70560728+emersonh-xyz@users.noreply.github.com>
2023-11-22 15:28:39 -0500

    Create ready.js

---
+++ src/json/ready.js
@@ -0,0 +1,21 @@
+const { ActivityType } = require("discord.js")
+
+module.exports = {
+    name: "ready",
+    once: true,
+
+    async execute(client) {
+
+        console.log(`${client.user.tag} is now ready`)
+
+        client.user.setPresence({ activities: [{ name: '/skullboard', type: ActivityType.Listening }], status: 'online' });
+
+        // Check for a new video on start-up
+        client.checkVideo()
+
+        // Check for new videos every set interval
+        setInterval(client.checkVideo, 600000);
+
+
+    }
+}
